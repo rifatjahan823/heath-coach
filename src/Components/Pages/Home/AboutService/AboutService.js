265099d61465e7b8bcd9cf7a8abda56ed80cd03c
rifatjahan823 <rifatjahan823@gmail.com>
2022-04-17 01:08:40 +0600

    add aboutservice section

--- src/Components/Pages/Home/AboutService/AboutService.js
+++ src/Components/Pages/Home/AboutService/AboutService.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLeaf } from '@fortawesome/free-solid-svg-icons'
 const AboutService = () => {
     return (
-        <div>
-            
+        <div className='container'>
+           <h2>Why people need a Health Coach</h2> 
+            <FontAwesomeIcon icon={faLeaf} />
+           <div className='row'>
+                <div className='col-lg-3 colo-md-6 col-sm-12'>
+                    <h2>Nutrition Strategies</h2>
+                </div>
+                <div className='col-lg-3 colo-md-6 col-sm-12'>
+                    <h2>Workout Routines</h2>
+                </div>
+                <div className='col-lg-3 colo-md-6 col-sm-12'>
+                    <h2>Individual Support</h2>
+                </div>
+                <div className='col-lg-3 colo-md-6 col-sm-12'>
+                    <h2>First-Hand Advice</h2>
+                </div>
+           </div>
         </div>
     );
 };
--- src/Components/Pages/Home/Home/Home.js
+++ src/Components/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AboutService from '../AboutService/AboutService';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AboutService></AboutService>
         </div>
     );
 };
